1164790a3f5a136c509c7c58e705cbdc3fb048fb
lukekarrys <lukekarrys@gmail.com>
2011-09-23 03:30:43 -0700

    Set ALLOW_EXTRAS and DISPLAY_CONTACT to false by default

--- includes/config/setup.php
+++ includes/config/setup.php
@@ -7,13 +7,13 @@ define("BASE_URL", "http://appurl");
 define("TOGGL_API_URL", "https://www.toggl.com/api/v2/");
 define("TOGGL_API_TOKEN", "TOKEN");
 
-define("ALLOW_EXTRAS", true);
+define("ALLOW_EXTRAS", false);
 
 define('DB_CONN', 'DB_CONN');
 define('DB_USER', 'DB_USER');
 define('DB_PASS', 'DB_PASS');
 
-define('DISPLAY_CONTACT', true);
+define('DISPLAY_CONTACT', false);
 define('CONTACT_NAME', 'CONTACT_NAME');
 define('CONTACT_EMAIL', 'CONTACT_EMAIL');
 define('CONTACT_PHONE', 'CONTACT_PHONE');
